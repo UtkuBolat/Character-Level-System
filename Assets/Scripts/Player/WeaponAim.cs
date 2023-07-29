c0c35d0476ed69a043983cd39ff680f9db643361
UtkuBolat <utkubolat@outlook.com>
2023-07-29 12:48:38 +0300

    A

--- Assets/Scripts/Player/WeaponAim.cs
+++ Assets/Scripts/Player/WeaponAim.cs
@@ -7,6 +7,11 @@ public class WeaponAim : MonoBehaviour
 {
     private Camera mainCam;
     private Vector3 mousePos;
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire;
+    private float timer;
+    public float timeBetweenFiring;
 
     private void Start()
     {
@@ -19,5 +24,21 @@ public class WeaponAim : MonoBehaviour
         Vector3 rotation = mousePos - transform.position;
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+        if (!canFire)
+        {
+            timer += Time.deltaTime;
+            if(timer > timeBetweenFiring)
+            {
+                canFire = true;
+                timer = 0;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0) && canFire)
+        {
+            canFire = false;
+            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        }
     }
 }
