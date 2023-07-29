d8cb9239c2dd9626edf367f301a5550a15dacb0a
UtkuBolat <utkubolat@outlook.com>
2023-07-29 12:08:24 +0300

    A

---
+++ Assets/Scripts/Player/WeaponAim.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WeaponAim : MonoBehaviour
+{
+    private Camera mainCam;
+    private Vector3 mousePos;
+
+    private void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = mousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+}
