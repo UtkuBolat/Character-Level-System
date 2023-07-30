3d6d4c01c03af646b2d772f3738ccac0e384547b
Mert Can Akbıyık <mertcanakbyk5@gmail.com>
2023-07-31 00:14:14 +0300

    update

--- Assets/Scripts/Weapon/WeaponAim.cs
+++ Assets/Scripts/Weapon/WeaponAim.cs
@@ -10,41 +10,21 @@ public class WeaponAim : MonoBehaviour
     public GameObject bullet;
     public Transform bulletTransform;
     public Transform weapon;
-    public bool canFire;
-    private float timer;
-    public float timeBetweenFiring;
+
 
     private void Start()
     {
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
-
-    private void FixedUpdate()
+    private void Update()
     {
+       
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 rotation = mousePos - transform.position;
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
 
-        if (!canFire)
-        {
-            timer += Time.deltaTime;
-            if(timer > timeBetweenFiring)
-            {
-                canFire = true;
-                timer = 0;
-            }
-        }
-
-       
-
-        if (Input.GetMouseButton(0) && canFire)
-        {
-            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
-        }
-
- 
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction = mousePosition - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
@@ -60,6 +40,14 @@ public class WeaponAim : MonoBehaviour
             localScale.y = +1f;
         }
         weapon.localScale = localScale;
-    }
 
+        if (Input.GetMouseButton(0))
+        {
+            
+            GameObject tempBullet = Instantiate(bullet, bulletTransform.position, Quaternion.identity) as GameObject;
+            Destroy(tempBullet,0.75f);
+
+        }
+    }
+  
 }
